4afc7b64c9d609542393a858ac93a92ea797f6b7
adi1096 <adithya961@outlook.com>
2019-07-31 22:05:58 +1000

    Fixed up last two functions

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -5,23 +5,20 @@ Go through it and change it until it runs.
 Remeber that all files must also pass the
 linter with no errors or warnings!
 """
-
 import string
-
 def getLetter(index):
     alphabet = string.ascii_lowercase + " "
-    return alphabet(index)
+    return alphabet[index]
 
 def week2exercise2():
-    index = [12, 2, 26, 7, 0, 12, 12, 4, 17]
-    wordArray = list(map(getLetter, index))
+    indices = (12, 2, 26, 7, 0, 12, 12, 4, 17)
+    wordArray = list(map(getLetter, indices))
     wordArray[0] = wordArray[0].upper()
     wordArray[1] = wordArray[1].upper()
     wordArray[3] = wordArray[3].upper()
-    secret_word="".join(wordArray)
+    secret_word= "".join(wordArray)
     print(secret_word)
     return secret_word
 
-
-if '__name__' == '__main__':
-    print (week2exercise2)
+if __name__ == '__main__':
+    print (week2exercise2())
